b519a1d124a70021d1d20b79851e5b179277a800
Katharina Höflich <37177103+kathoef@users.noreply.github.com>
2021-05-26 17:22:49 +0200

    Simplify for NESH deployment...

--- logger.sh
+++ logger.sh
@@ -2,12 +2,14 @@
 
 INTERVAL=3600
 
-LOGS_DIR=morpheus_logs
+LOGS_DIR=$PWD/nesh_logs
 mkdir -p ${LOGS_DIR}
 
 # https://stackoverflow.com/a/61643945
-while sleep $(echo ${INTERVAL}-`date "+%s"`%${INTERVAL} | bc); do
- MODE=1 INSTALL_DIR=$PWD bash starter.sh > ${LOGS_DIR}/conda-$(date +%Y%m%d%H%M%S).log 2>&1
- MODE=2 bash starter.sh > ${LOGS_DIR}/singularity-$(date +%Y%m%d%H%M%S).log 2>&1
-done
+#while sleep $(echo ${INTERVAL}-`date "+%s"`%${INTERVAL} | bc); do
+ MODE=2 IMAGE_DIR=/gxfs_home/geomar/smomw260/jupyter.sif bash starter.sh > ${LOGS_DIR}/singularity-home-$(date +%Y%m%d%H%M%S).log 2>&1
+ MODE=2 IMAGE_DIR=/gxfs_work1/geomar/smomw260/jupyter.sif bash starter.sh > ${LOGS_DIR}/singularity-work-$(date +%Y%m%d%H%M%S).log 2>&1
+ MODE=1 INSTALL_DIR=/gxfs_home/geomar/smomw260/miniconda3 bash starter.sh > ${LOGS_DIR}/conda-home-$(date +%Y%m%d%H%M%S).log 2>&1
+ MODE=1 INSTALL_DIR=/gxfs_work1/geomar/smomw260/miniconda3 bash starter.sh > ${LOGS_DIR}/conda-work-$(date +%Y%m%d%H%M%S).log 2>&1
+#done
 
--- starter.sh
+++ starter.sh
@@ -7,17 +7,22 @@ if [ "$MODE" == "1" ]; then
 if [ -z "${INSTALL_DIR}" ]; then echo "INSTALL_DIR not provided... exiting."; exit; fi
 
 hostname
-date +%F_%T
-/usr/bin/time bash -c "source ${INSTALL_DIR}/miniconda3/etc/profile.d/conda.sh && conda activate base && \
+date +%F_%T_%N
+#/usr/bin/time 
+bash -c "source ${INSTALL_DIR}/etc/profile.d/conda.sh && conda activate base && \
 jupyter lab --no-browser --ServerApp.shutdown_no_activity_timeout=60"
-date +%F_%T
+date +%F_%T_%N
 
 elif [ "$MODE" == "2" ]; then
 
+if [ -z "${IMAGE_DIR}" ]; then echo "IMAGE_DIR not provided... exiting."; exit; fi
+module load singularity/3.6.4
+
 hostname
-date +%F_%T
-/usr/bin/time bash -c "singularity run jupyter.sif jupyter lab --no-browser --ServerApp.shutdown_no_activity_timeout=60"
-date +%F_%T
+date +%F_%T_%N
+#/usr/bin/time 
+bash -c "singularity run ${IMAGE_DIR} jupyter lab --no-browser --ServerApp.shutdown_no_activity_timeout=60"
+date +%F_%T_%N
 
 fi
 
