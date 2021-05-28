77164833f89bab434ea70206ea9e28168293126d
Katharina Höflich <37177103+kathoef@users.noreply.github.com>
2021-05-28 10:41:47 +0200

    Add nesh-fe3 experiment logs

--- logger.sh
+++ logger.sh
@@ -2,14 +2,22 @@
 
 INTERVAL=3600
 
-LOGS_DIR=$PWD/nesh_logs
-mkdir -p ${LOGS_DIR}
-
-# https://stackoverflow.com/a/61643945
-#while sleep $(echo ${INTERVAL}-`date "+%s"`%${INTERVAL} | bc); do
+function nesh_experiments {
  MODE=2 IMAGE_DIR=/gxfs_home/geomar/smomw260/jupyter.sif bash starter.sh > ${LOGS_DIR}/singularity-home-$(date +%Y%m%d%H%M%S).log 2>&1
  MODE=2 IMAGE_DIR=/gxfs_work1/geomar/smomw260/jupyter.sif bash starter.sh > ${LOGS_DIR}/singularity-work-$(date +%Y%m%d%H%M%S).log 2>&1
  MODE=1 INSTALL_DIR=/gxfs_home/geomar/smomw260/miniconda3 bash starter.sh > ${LOGS_DIR}/conda-home-$(date +%Y%m%d%H%M%S).log 2>&1
  MODE=1 INSTALL_DIR=/gxfs_work1/geomar/smomw260/miniconda3 bash starter.sh > ${LOGS_DIR}/conda-work-$(date +%Y%m%d%H%M%S).log 2>&1
-#done
+}
+
+LOGS_DIR=$PWD/nesh-fe3_logs
+mkdir -p ${LOGS_DIR}
+
+while sleep $(echo ${INTERVAL}-`date "+%s"`%${INTERVAL} | bc); do
+ nesh_experiments
+done
+
+#
+# References
+# https://stackoverflow.com/a/61643945
+#
 
